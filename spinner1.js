dad55587291b7e0a69ee96e11c1c77ccdcc505f3
Joseph Shin <seoung.shin@ontariotechu.net>
2021-09-21 10:58:12 -0400

    initial commit

---
+++ spinner1.js
@@ -0,0 +1,28 @@
+// WONT WORK ON CODERUNNER EXTENSION. RUN ON NODE
+process.stdout.write('hello from spinner1.js... \rheyyy\n'); // > heyyy from spinner1.js...
+
+/* coderunner extension dosent output correctly (hello from spinner1.js...
+heyyy). node in the terminal does. /r brings cursor to beginning and should overwrite the hello. */
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r |   ');
+}, 900);
+
